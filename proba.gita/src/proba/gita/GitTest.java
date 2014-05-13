c67c2ba6f8cf92f2bcbeef55fa82950ec6f695b6
aomanovic <ao15155@etf.unsa.ba>
2014-05-13 18:32:12 +0200

    Update GitTest.java

--- proba.gita/src/proba/gita/GitTest.java
+++ proba.gita/src/proba/gita/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Github is fun.");
+		System.out.println("Evo nova linija preko githuba, a ne eclipsa.");
 
 	}
 
